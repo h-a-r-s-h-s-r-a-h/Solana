968b5fcafcba9ff87a186c9c16d4c14cc48361fe
Harsh <harshvirat894@gmail.com>
2025-02-05 04:16:29 +0530

    Solana

--- Project01/generate-keypair/loading-keypair.ts
+++ Project01/generate-keypair/loading-keypair.ts
@@ -3,7 +3,8 @@ import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
+console.log(`The public key is :- `, keypair.publicKey.toBase58());
+console.log(`The private key is :- `, keypair.secretKey);
 console.log(
-  `✅ Finished! We've loaded our secret key securely, using an env file!`,
-  keypair.secretKey
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
 );
