915333eef03811cc35f63b6ff78b9c499abdb0c7
Harsh <harshvirat894@gmail.com>
2025-02-13 01:50:16 +0530

    Solana

---
+++ Project04/token-program/pages/_app.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./globals.css"; // ✅ Import global styles here
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
